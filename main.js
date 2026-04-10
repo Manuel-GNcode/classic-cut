aaf4b4ab90b08f49e31c5245b6dafdf390731ce0
GNcode <gngalarga027@gmail.com>
2026-04-10 13:04:52 -0500

    feat: implement GSAP entry animations and add element IDs for hero section orchestration

--- main.js
+++ main.js
@@ -89,3 +89,34 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+
+// Animación de entrada
+const tlFadeId = gsap.timeline({
+  defaults: {
+    ease: "sine.out",
+    duration: 0.5,
+    opacity: 0
+  }
+});
+
+tlFadeId.from('#hero-img', {
+  filter: 'brightness(0.5) grayscale(0)',
+  scale: 1.1,
+  duration: 1,
+}).from('#hero-title_first', {
+  x: 100,
+  ease: 'power3.out'
+}).from('#hero-title_second', {
+  x: -100,
+  ease: 'power3.out'
+}, '<').from('#hero-description', {
+  y: 20,
+}).from('#hero-button', {
+  y: 20,
+}, '<').from('#hero-since', {
+
+}, '<').from('#header', {
+  y: -50,
+}, '<').from('#hero-scroll-down,#whatsapp-fab', {
+
+}, '<')
